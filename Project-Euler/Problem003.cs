37cea2e1313b0e2bb8243aee5d136455ca91699c
Kai Taylor <KaiGH@users.noreply.github.com>
2018-07-27 22:44:23 +0100

    Problem 003

---
+++ Project-Euler/Problem003.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+// This program finds the largest prime factor of the number 600851475143.
+// The program prints each prime number starting from 1 until it reaches the maximum prime number.
+
+namespace ProjectEuler
+{
+    class Problem003
+    {
+        public static void Solution()
+        {
+            double number = 600851475143;
+            // Count until divisable number reached
+            for (double i = 0; i < number / 2; i++)
+            {
+                // Check if i divided by number is a whole number
+                if (number % i == 0)
+                {
+                    // Check if i is a prime number
+                    if (IsPrime(i) == true)
+                    {
+                        // Print prime numbers
+                        Console.WriteLine(i);
+                    }
+                }
+            }
+        }
+
+        static bool IsPrime(double i)
+        {
+            for (Int64 n = 2; n < i; n++)
+            {
+                if (i % n == 0)
+                {
+                    return (false);
+                }
+            }
+            return (true);
+        }
+    }
+}
--- Project-Euler/Program.cs
+++ Project-Euler/Program.cs
@@ -11,28 +11,31 @@ namespace ProjectEuler
             Console.WriteLine("Enter a number to run the Project Euler problem");
             Console.WriteLine("001 - Multiples of 3 and 5");
             Console.WriteLine("002 - Even Fibonacci numbers");
-            input();
-        }
-
-        static void input()
-        {
-            int UserInput = Convert.ToInt32(Console.ReadLine());
-            if (UserInput < 3 && UserInput > 0)
+            Console.WriteLine("003 - Largest prime factor");
+            Console.WriteLine("0 to Exit");
+            
+            while (true)
             {
-                output(UserInput);
+                switch (Convert.ToInt32(Console.ReadLine()))
+                {
+                    case 1:
+                        Problem001.Solution();
+                        break;
+                    case 2:
+                        Problem002.Solution();
+                        break;
+                    case 3:
+                        Problem003.Solution();
+                        break;
+                    case 0:
+                        System.Environment.Exit(1);
+                        break;
+                    default:
+                        Console.WriteLine("Invalid input. Please try again.");
+                        Main();
+                        break;
+                }
             }
-            else
-            {
-                Console.WriteLine("Invalid input. Please try again.");
-                input();
-            }
-        }
-
-        static void output(int UserInput)
-        {
-            if (UserInput == 1) { Problem001.Solution(); }
-            if (UserInput == 2) { Problem002.Solution(); }
-            input();
         }
     }
 }
